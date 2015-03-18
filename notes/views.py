752e468e905c73787d92510926b107d71db34fbe
Eleyine Zarour <eleyine@gmail.com>
2015-03-18 15:41:47 -0400

    make use of render shortcut

--- notes/views.py
+++ notes/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
 
 from notes.models import Note
  
 def index(request):
-    template = loader.get_template('notes/index.html')
-    context = RequestContext(request, {
+    context = {
         'notes': Note.objects.all(),
-    })
-    return HttpResponse(template.render(context))
+    }
+    return render(request, 'notes/index.html', context)
 
 def archive_index(request):
     return HttpResponse("You're looking at the archive.")
