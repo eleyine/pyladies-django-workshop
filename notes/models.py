acae67b2a55965280648e7b722be98670ec1d47e
Eleyine Zarour <eleyine@gmail.com>
2015-03-18 07:42:43 -0400

    more note field examples

--- notes/models.py
+++ notes/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Note(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=100, default='')
+    content = models.TextField(default='')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    is_pinned = models.BooleanField(default=False)
+    is_archived = models.BooleanField(default=False)
 
     def __unicode__(self):
         return '{0}'.format(self.title)
