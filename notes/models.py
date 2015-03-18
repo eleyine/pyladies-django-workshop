dfe18772bdcaedd87ed222bdad7b2ff61485ecb8
Eleyine Zarour <eleyine@gmail.com>
2015-03-18 07:41:21 -0400

    model __unicode__ functions

--- notes/models.py
+++ notes/models.py
@@ -4,9 +4,17 @@ class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
 
+    def __unicode__(self):
+        return '{0}'.format(self.title)
+
 class Tag(models.Model):
     keyword = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return '#{0}'.format(self.keyword)
 
 class NoteCollection(models.Model):
     name = models.CharField(max_length=50)
 
+    def __unicode__(self):
+        return '{0}'.format(self.name)
