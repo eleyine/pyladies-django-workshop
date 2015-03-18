ac5cf444dc44fdb31b6a6a5deba60ab7d4841db5
Eleyine Zarour <eleyine@gmail.com>
2015-03-18 10:45:15 -0400

    register notes app in admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from notes.models import Note, Tag, NoteCollection
 
-# Register your models here.
+admin.site.register(Note)
+admin.site.register(Tag)
+admin.site.register(NoteCollection)
