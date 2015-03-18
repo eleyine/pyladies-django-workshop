c35c304cc628ffa9f6812793f3c605b8d657984a
Eleyine Zarour <eleyine@gmail.com>
2015-03-18 13:10:26 -0400

    first basic view

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+ 
+def index(request):
+    return HttpResponse("Hello, world. You're at the notes index.")
--- pyladies_django_workshop/urls.py
+++ pyladies_django_workshop/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', 'pyladies_django_workshop.views.home', name='home'),
+    url(r'^', include('notes.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
