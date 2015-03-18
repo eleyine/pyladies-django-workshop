f260ba43f7a84f0d255900c20865c7dfe7743863
Eleyine Zarour <eleyine@gmail.com>
2015-03-18 13:23:53 -0400

    wiring some more urls

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,16 @@ from django.conf.urls import patterns, url
 from notes import views
  
 urlpatterns = patterns('',
+    # ex: /
     url(r'^$', views.index, name='index'),
+    # ex: /archive.
+    url(r'^archive/$', views.archive_index, name='archive'),
+    # ex: /pinned/
+    url(r'^pinned/$', views.pinned_index, name='pinned'),
+    # ex: /5/
+    url(r'^(?P<note_id>\d+)/$', views.detail, name='detail'),
+    # ex: /create/
+    url(r'^create$', views.create_note, name='create_note'),
+    # ex: /5/edit
+    url(r'^(?P<note_id>\d+)/edit$', views.edit_note, name='edit_note'),
 )
